f40649a144089db5a123e727b3b6e2d44881802d
pouiscakes <louisrlin@gmail.com>
2017-04-17 15:43:08 -0700

    Added the socket connection code

--- src/Retrieve.java
+++ src/Retrieve.java
@@ -1,67 +1,42 @@
-public class Retrieve {
+//java socket client example
+import java.io.*;
+import java.net.*;
 
+public class Retrieve {
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket();
+        String host = "www.google.com";
+        PrintWriter s_out = null;
+        BufferedReader s_in = null;
+
+        try {
+            s.connect(new InetSocketAddress(host, 80));
+            System.out.println("Connected");
+
+            //writer for socket
+            s_out = new PrintWriter(s.getOutputStream(), true);
+            //reader for socket
+            s_in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        }
+
+        //Host not found
+        catch (UnknownHostException e) {
+            System.err.println("Don't know about host : " + host);
+            System.exit(1);
+        }
+
+        //Send message to server
+        String message = "GET / HTTP/1.1\r\n\r\n";
+        s_out.println(message);
+
+        System.out.println("Message send");
+
+        //Get response from server
+        String response;
+        while ((response = s_in.readLine()) != null) {
+            System.out.println(response);
+        }
+    }
 }
-//    /**
-//     * This method listens and handles any of the data streamed from other hosts
-//     * @param args
-//     */
-//    public void listener(String[] args) {
-//        ServerSocket serverSocket;
-//        Socket socket;
-//        Thread lindaTerminal;
-//
-//        try {
-//            HOSTNAME = args[0];
-//        } catch (ArrayIndexOutOfBoundsException e) {
-//            System.out.println("Please specify the host name when running the executable");
-//            return;
-//        }
-//
-//        try {
-//            // Create a TCP server socket on a random available port
-//            while(true) {
-//                PORT_NUMBER = ThreadLocalRandom.current().nextInt(1024, 65535 + 1);
-//                try {
-//                    serverSocket = new ServerSocket(PORT_NUMBER);
-//                    break;
-//                } catch(IOException e) {
-//                    continue;
-//                }
-//            }
-//
-//            // Get & display IP of the current machine
-//            IP_ADDRESS = InetAddress.getLocalHost().getHostAddress();
-//            System.out.println(IP_ADDRESS + " at port number: " + PORT_NUMBER);
-//
-//            // Remove old tuple space if it exists
-//            String tupleSpaceFilePath = "/tmp/" + LOGIN + "/linda/" + HOSTNAME + "/tuples/tuples.txt";
-//            File dir = new File(tupleSpaceFilePath);
-//            Files.deleteIfExists(dir.toPath());
-//
-//            // Add this host to the list of hosts in our network
-//            listOfHosts += (HOSTNAME + " " + IP_ADDRESS + " " + PORT_NUMBER + ",");
-//            add();
-//
-//            // Create a new thread to accept Linda Terminal Commands
-//            lindaTerminal = new Thread(new P1());
-//            lindaTerminal.start();
-//
-//            // Listen for new socket connections to from hosts that request it
-//            while (true) {
-//                socket = serverSocket.accept();
-//
-//                // Read the input stream of messages from other hosts
-//                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//                String dataStreamCommand = br.readLine();
-//                parseDataStreamCommand(dataStreamCommand);
-//
-//                socket.close();
-//            }
-//
-//            // Close the sockets?
-//            // TODO: CHeck for when input stream is null. Once it is, then we close the socket
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+
 
